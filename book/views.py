184d42d3abbad64d07bff1950729eca86fa702da
AHB <amirhossein.b.cr7@gmail.com>
2025-02-08 09:01:28 +0330

    creating a book list and show it

--- book/views.py
+++ book/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+book_item=[
+    {"id":1,"name":"compound effect","writer":"darren hardey"}
+]
+
+def books(request):
+    data=""
+    for item in book_item:
+        data=data+f"id:{item['id']} , name:{item['name']} , writer:{item['writer']}"+"<br>"
+    return HttpResponse(data)
+def detail(request):
+    return HttpResponse("book details")
